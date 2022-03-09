5a188a4e56b1261f5dcfc141ebae994bdbc4b6c9
symphony <jciurc@gmail.com>
2022-03-09 09:52:57 +0000

    Initial commit with all js files so far

---
+++ numbers.js
@@ -0,0 +1,43 @@
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+const example = 123456789;
+console.log(example);
+
+#what is this 
+/* comment ajslkdfj;lajdf */
+let string = "this is a string"
+
+function asdfjl(asdf,) {
+
+  aflakjsd;
+  return;
+}asdf 
+
+  
